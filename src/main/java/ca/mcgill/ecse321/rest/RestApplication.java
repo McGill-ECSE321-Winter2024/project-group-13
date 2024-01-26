4657f906b17fab4a8be4d3bb8ad2f4210eb4b145
Mohamed Abdelrahman <mohamed.abdelrahmanibrahim@mail.mcgill.ca>
2024-01-26 11:03:59 -0500

    Fixing the beautiful beautiful set up

--- src/main/java/ca/mcgill/ecse321/rest/RestApplication.java
+++ src/main/java/ca/mcgill/ecse321/rest/RestApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class    ProjectApplication {
+public class RestApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ProjectApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(RestApplication.class, args);
+	}
 
 }
