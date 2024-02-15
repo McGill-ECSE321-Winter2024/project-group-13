0f046dd16bb7687245da9b9d8be1f83bb0482248
philippeapra <philippeaprahamian6@gmail.com>
2024-02-15 18:03:20 -0500

    sports center testing done

--- src/main/java/ca/mcgill/ecse321/rest/dao/SportCenterRepository.java
+++ src/main/java/ca/mcgill/ecse321/rest/dao/SportCenterRepository.java
@@ -3,7 +3,7 @@ package ca.mcgill.ecse321.rest.dao;
 import ca.mcgill.ecse321.rest.models.SportCenter;
 import org.springframework.data.repository.CrudRepository;
 
-public interface SportsCenterRepository extends CrudRepository<SportCenter, String>{
+public interface SportCenterRepository extends CrudRepository<SportCenter, String>{
 
     SportCenter findSportCenterById(String id);
 
