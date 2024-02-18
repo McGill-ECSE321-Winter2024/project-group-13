1d3b690d4c87898ff4ddc60c0f18f23ce2a82340
Achraf Ghellach <a.ghellach@gmail.com>
2024-02-18 18:31:15 -0500

    remove un used imports

--- src/test/java/ca/mcgill/ecse321/rest/dao/CustomerRepositoryTests.java
+++ src/test/java/ca/mcgill/ecse321/rest/dao/CustomerRepositoryTests.java
@@ -8,7 +8,6 @@ import ca.mcgill.ecse321.rest.models.Customer;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
