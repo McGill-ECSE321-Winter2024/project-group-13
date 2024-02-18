1cbc30ddf0a9efb1b362125510d3dc5fda904424
Mohamed Abdelrahman <mohamed.abdelrahmanibrahim@mail.mcgill.ca>
2024-02-18 10:05:54 -0500

    Clean up courseRepositoryTests file

--- src/test/java/ca/mcgill/ecse321/rest/dao/CourseRepositoryTests.java
+++ src/test/java/ca/mcgill/ecse321/rest/dao/CourseRepositoryTests.java
@@ -17,20 +17,26 @@ public class CourseRepositoryTests {
   @Autowired private CourseRepository courseRepository;
 
   /**
-   * Makes a course before every test, setting most of its attributes
+   * Sets the attributes of a course, saves it to the database, and ensures persistence.
    *
+   * @param course The course whose attributes are to be set and saved.
+   * @param name The new name for the course.
+   * @param description The new description for the course.
+   * @param level The new level for the course.
+   * @param courseStartDate The new start date for the course.
+   * @param courseEndDate The new end date for the course.
+   * @param courseState The new state for the course.
    * @author Mohamed Abdelrahman
    */
-  @BeforeEach
-  public void makeCourse() {
-    String name = "Health Plus";
-    String description = "The best sports center";
-    Course.Level level = Course.Level.Advanced;
-    Timestamp courseStartDate = new Timestamp(2024, 1, 1, 6, 15, 0, 0);
-    Timestamp courseEndDate = new Timestamp(2024, 1, 1, 6, 15, 0, 0);
-    Course.CourseState courseState = Course.CourseState.AwaitingApproval;
-
-    Course course = new Course();
+  public void setAttributes(
+      Course course,
+      String name,
+      String description,
+      Course.Level level,
+      Timestamp courseStartDate,
+      Timestamp courseEndDate,
+      Course.CourseState courseState) {
+    // Set the new attributes for the course
     course.setName(name);
     course.setDescription(description);
     course.setLevel(level);
@@ -38,8 +44,31 @@ public class CourseRepositoryTests {
     course.setCourseEndDate(courseEndDate);
     course.setCourseState(courseState);
 
-    // Save Course
+    // Save the modified course to the database
     courseRepository.save(course);
+  }
+
+  /**
+   * Initializes and creates a sample course with predefined attributes for testing purposes. This
+   * method is executed before each test method annotated with {@code @Test}.
+   *
+   * @author Mohamed Abdelrahman
+   */
+  @BeforeEach
+  public void makeCourse() {
+    // Define predefined attributes for the sample course
+    String name = "Health Plus";
+    String description = "The best sports center";
+    Course.Level level = Course.Level.Advanced;
+    Timestamp courseStartDate = new Timestamp(2024, 1, 1, 6, 15, 0, 0);
+    Timestamp courseEndDate = new Timestamp(2024, 1, 1, 6, 15, 0, 0);
+    Course.CourseState courseState = Course.CourseState.AwaitingApproval;
+
+    // Create a new Course instance
+    Course course = new Course();
+
+    // Set attributes and save the course to the database
+    setAttributes(course, name, description, level, courseStartDate, courseEndDate, courseState);
   }
 
   /**
@@ -72,6 +101,7 @@ public class CourseRepositoryTests {
     Timestamp courseEndDate = new Timestamp(2024, 1, 1, 6, 15, 0, 0);
     Course.CourseState courseState = Course.CourseState.AwaitingApproval;
 
+    // check attribute values
     assertEquals(name, course.getName());
     assertEquals(description, course.getDescription());
     assertEquals(level, course.getLevel());
@@ -81,15 +111,19 @@ public class CourseRepositoryTests {
   }
 
   /**
-   * Meant to ensure that we can read and update the attributes in existing courses
+   * Test goal: Tests the functionality of editing course attributes. This test verifies that the
+   * attributes of a course can be modified, saved to the database, and retrieved correctly with the
+   * updated values.
    *
    * @author Mohamed Abdelrahman
    */
   @Test
   public void testEditCourseAttributes() {
-    // get course
+    // Retrieve an existing course from the database
     Course course = courseRepository.findCourseByName("Health Plus");
     assertNotNull(course);
+
+    // Define new attribute values
     String name = "New Center";
     String description = "The best yoga course";
     Course.Level level = Course.Level.Beginner;
@@ -97,19 +131,18 @@ public class CourseRepositoryTests {
     Timestamp courseEndDate = new Timestamp(2024, 4, 1, 10, 10, 15, 0);
     Course.CourseState courseState = Course.CourseState.Approved;
 
-    course.setName(name);
-    course.setDescription(description);
-    course.setLevel(level);
-    course.setCourseStartDate(courseStartDate);
-    course.setCourseEndDate(courseEndDate);
-    course.setCourseState(courseState);
+    // Set the new attribute values for the course
+    setAttributes(course, name, description, level, courseStartDate, courseEndDate, courseState);
 
-    // Save Course
-    courseRepository.save(course);
+    // Ensure that the course with the original name is not found
     Course course1 = courseRepository.findCourseByName("Health Plus");
     assertNull(course1);
+
+    // Retrieve the modified course by its new name
     course = courseRepository.findCourseByName(name);
     assertNotNull(course);
+
+    // Assert that the attributes of the course match the expected values
     assertEquals(name, course.getName());
     assertEquals(description, course.getDescription());
     assertEquals(level, course.getLevel());
@@ -118,30 +151,49 @@ public class CourseRepositoryTests {
     assertEquals(courseState, course.getCourseState());
   }
 
-  /** */
+  /**
+   * Test goal: Tests the functionality of finding courses by their approval state. This test
+   * verifies that courses with a specific approval state are retrieved correctly from the database
+   * and that their attributes match the expected values.
+   *
+   * @author Mohamed Abdelrahman
+   */
   @Test
   public void testFindCoursesByCourseState() {
+    // Set the approval state for testing
     Course.CourseState courseState = Course.CourseState.Approved;
+
+    // Create and save the first course with the specified approval state
     Course course = courseRepository.findCourseByName("Health Plus");
     course.setName("First Course");
     course.setCourseState(courseState);
     courseRepository.save(course);
+
+    // Create another course with the same name but different attributes
     makeCourse();
     Course course1 = courseRepository.findCourseByName("Health Plus");
     course1.setName("Second Course");
     course1.setCourseState(courseState);
     courseRepository.save(course1);
+
+    // Ensure that the IDs of the two courses are not equal
     assertNotEquals(course.getId(), course1.getId());
 
+    // Create a list of locally approved courses
     List<Course> myApprovedCourses = new ArrayList<>();
     myApprovedCourses.add(course);
     myApprovedCourses.add(course1);
+
+    // Retrieve approved courses from the system (database)
     List<Course> systemApprovedCourses = courseRepository.findCoursesByCourseState(courseState);
-    // Still needs implementation
-    //        for (int i=0;i<systemApprovedCourses.size();i++){
-    //            System.out.println(myApprovedCourses.contains(systemApprovedCourses.get(i)));
-    //        }
+
+    // Assert that the sizes of the two lists are equal
     assertEquals(myApprovedCourses.size(), systemApprovedCourses.size());
+
+    // Iterate over both lists and compare the IDs of the courses
+    for (int i = 0; i < systemApprovedCourses.size(); i++) {
+      assertEquals(myApprovedCourses.get(i).getId(), systemApprovedCourses.get(i).getId());
+    }
   }
 
   /**
