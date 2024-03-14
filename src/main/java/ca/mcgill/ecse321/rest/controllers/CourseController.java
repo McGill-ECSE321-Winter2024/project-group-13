f9594e77544814c4210f13ccbd7a670a1c6b3321
Mohamed Abdelrahman <mohamed.abdelrahmanibrahim@mail.mcgill.ca>
2024-03-14 15:12:40 -0400

    Awaiting authentication

--- src/main/java/ca/mcgill/ecse321/rest/controllers/CourseController.java
+++ src/main/java/ca/mcgill/ecse321/rest/controllers/CourseController.java
@@ -1,18 +1,35 @@
 package ca.mcgill.ecse321.rest.controllers;
+import ca.mcgill.ecse321.rest.PersonSession;
 import ca.mcgill.ecse321.rest.dto.CourseDTO;
+import ca.mcgill.ecse321.rest.dto.SessionDTO;
 import ca.mcgill.ecse321.rest.models.Course;
+import ca.mcgill.ecse321.rest.models.Owner;
+import ca.mcgill.ecse321.rest.services.AuthenticationService;
 import ca.mcgill.ecse321.rest.services.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-//@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "*")
 @RestController
 public class CourseController {
+    @Autowired
+    private CourseService courseService;
+    @Autowired
+    private AuthenticationService authenticationService;
     @PostMapping(value = { "/courses", "/courses/" })
     @ResponseStatus(HttpStatus.CREATED)
     public CourseDTO createCourse(@RequestBody CourseDTO courseDTO) {
-        Course createdCourse = CourseService.createCourse(courseDTO);
+        Course createdCourse = courseService.createCourse(courseDTO);
         return new CourseDTO(createdCourse);
+    }
+    @PostMapping(value = { "/courses/{course_id}/approve", "/courses/{course_id}/approve/" })
+    @ResponseStatus(HttpStatus.OK)
+    public CourseDTO approveCourse(@PathVariable String course_id, @RequestBody SessionDTO session) {
+//        PersonSession session= courseService.verifyTokenAndGetUser();
+//        Course approvedCourse = courseService.approveCourse(course_id, owner);
+//        return new CourseDTO(approvedCourse);
+        return null;
     }
 
 }
--- src/main/java/ca/mcgill/ecse321/rest/services/AuthenticationService.java
+++ src/main/java/ca/mcgill/ecse321/rest/services/AuthenticationService.java
@@ -6,9 +6,9 @@ import ca.mcgill.ecse321.rest.models.Customer;
 import ca.mcgill.ecse321.rest.models.Instructor;
 import ca.mcgill.ecse321.rest.models.Owner;
 import ca.mcgill.ecse321.rest.models.Person;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+//import io.jsonwebtoken.Claims;
+//import io.jsonwebtoken.Jwts;
+//import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
