38fa336902d32a2c3e61005e9c164e2053af618e
philippeapra <philippeaprahamian6@gmail.com>
2024-03-15 15:49:14 -0400

    sportcenter DTO

--- src/main/java/ca/mcgill/ecse321/rest/dto/SportCenterDTO.java
+++ src/main/java/ca/mcgill/ecse321/rest/dto/SportCenterDTO.java
@@ -1,3 +1,26 @@
 package ca.mcgill.ecse321.rest.dto;
 
-public class SportCenterDTO {}
+import ca.mcgill.ecse321.rest.models.*;
+import jakarta.persistence.OneToOne;
+
+public class SportCenterDTO {
+    private String id;
+
+    private String address;
+    private String name;
+    private Schedule schedule;
+
+    public SportCenterDTO(SportCenter sportCenter){
+        this.id=sportCenter.getId();
+        this.address=sportCenter.getAddress();
+        this.name=sportCenter.getName();
+        this.schedule=sportCenter.getSchedule();
+    }
+
+    public String getId() {
+        return id;
+    }
+    public String getAddress(){return address;}
+    public String getName() {return name;}
+    public Schedule getSchedule(){return schedule;}
+}
