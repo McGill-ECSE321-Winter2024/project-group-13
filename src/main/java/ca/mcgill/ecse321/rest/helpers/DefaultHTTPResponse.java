be217aab5dc6951e8104f9d7f8c6b4b95e18a799
Mohamed Abdelrahman <mohamed.abdelrahmanibrahim@mail.mcgill.ca>
2024-03-16 17:15:42 -0400

    Change HTTP

--- src/main/java/ca/mcgill/ecse321/rest/helpers/DefaultHTTPResponse.java
+++ src/main/java/ca/mcgill/ecse321/rest/helpers/DefaultHTTPResponse.java
@@ -1,40 +1,48 @@
 package ca.mcgill.ecse321.rest.helpers;
 
 import ca.mcgill.ecse321.rest.dto.auth.SessionDTO;
-import ca.mcgill.ecse321.rest.dto.http.ErrorHTTPDTO;
+import ca.mcgill.ecse321.rest.dto.http.HTTPDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 public class DefaultHTTPResponse {
-    public static ResponseEntity<ErrorHTTPDTO> unauthorized(String message) {
-        return new ResponseEntity<>(new ErrorHTTPDTO(message), HttpStatus.UNAUTHORIZED);
+    public static ResponseEntity<HTTPDTO> unauthorized(String message) {
+        return new ResponseEntity<>(new HTTPDTO(message), HttpStatus.UNAUTHORIZED);
     }
 
-    public static ResponseEntity<ErrorHTTPDTO> unauthorized() {
-        return new ResponseEntity<>(new ErrorHTTPDTO("Unauthorized"), HttpStatus.UNAUTHORIZED);
+    public static ResponseEntity<HTTPDTO> unauthorized() {
+        return new ResponseEntity<>(new HTTPDTO("Unauthorized"), HttpStatus.UNAUTHORIZED);
+    }
+    
+    public static ResponseEntity<HTTPDTO> unauthorized(String message) {
+        return new ResponseEntity<>(new HTTPDTO(message), HttpStatus.UNAUTHORIZED);
     }
 
-    public static ResponseEntity<ErrorHTTPDTO> forbidden(String message) {
-        return new ResponseEntity<>(new ErrorHTTPDTO(message), HttpStatus.FORBIDDEN);
+    public static ResponseEntity<HTTPDTO> unauthorized() {
+        return new ResponseEntity<>(new HTTPDTO("Unauthorized"), HttpStatus.UNAUTHORIZED);
     }
 
-    public static ResponseEntity<ErrorHTTPDTO> forbidden() {
-        return new ResponseEntity<>(new ErrorHTTPDTO("Forbidden"), HttpStatus.FORBIDDEN);
+    public static ResponseEntity<HTTPDTO> forbidden(String message) {
+        return new ResponseEntity<>(new HTTPDTO(message), HttpStatus.FORBIDDEN);
     }
 
-    public static ResponseEntity<ErrorHTTPDTO> notFound(String message) {
-        return new ResponseEntity<>(new ErrorHTTPDTO(message), HttpStatus.NOT_FOUND);
+    public static ResponseEntity<HTTPDTO> forbidden() {
+        return new ResponseEntity<>(new HTTPDTO("Forbidden"), HttpStatus.FORBIDDEN);
     }
 
-    public static ResponseEntity<ErrorHTTPDTO> notFound() {
-        return new ResponseEntity<>(new ErrorHTTPDTO("Not Found"), HttpStatus.NOT_FOUND);
+    public static ResponseEntity<HTTPDTO> notFound(String message) {
+        return new ResponseEntity<>(new HTTPDTO(message), HttpStatus.NOT_FOUND);
     }
 
-    public static ResponseEntity<ErrorHTTPDTO> badRequest(String message) {
-        return new ResponseEntity<>(new ErrorHTTPDTO(message), HttpStatus.BAD_REQUEST);
+    public static ResponseEntity<HTTPDTO> notFound() {
+        return new ResponseEntity<>(new HTTPDTO("Not Found"), HttpStatus.NOT_FOUND);
     }
 
-    public static ResponseEntity<ErrorHTTPDTO> badRequest() {
-        return new ResponseEntity<>(new ErrorHTTPDTO("Bad Request"), HttpStatus.BAD_REQUEST);
+    public static ResponseEntity<HTTPDTO> badRequest(String message) {
+        return new ResponseEntity<>(new HTTPDTO(message), HttpStatus.BAD_REQUEST);
+    }
+
+    public static ResponseEntity<HTTPDTO> badRequest() {
+        return new ResponseEntity<>(new HTTPDTO("Bad Request"), HttpStatus.BAD_REQUEST);
     }
 }
