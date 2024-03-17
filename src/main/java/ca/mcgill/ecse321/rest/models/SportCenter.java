5b863d45df1d770fff4b6f93c9321883ec0374cf
Achraf Ghellach <a.ghellach@gmail.com>
2024-03-17 13:45:07 -0400

    fix to null pointer in authentication tests

--- src/main/java/ca/mcgill/ecse321/rest/models/SportCenter.java
+++ src/main/java/ca/mcgill/ecse321/rest/models/SportCenter.java
@@ -71,6 +71,9 @@ public class SportCenter {
   public String getId() {
     return id;
   }
+  public void setId(String id) {
+    this.id = id;
+  }
 
   public String toString() {
     return super.toString()
