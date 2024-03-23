450fbc57050c2b4b38f9d8d06c9825752b3190eb
TeddyEl-Husseini <143761330+TeddyEl-Husseini@users.noreply.github.com>
2024-03-23 13:21:45 -0400

    Updated DTOs

--- src/main/java/ca/mcgill/ecse321/rest/dto/CustomerDTO.java
+++ src/main/java/ca/mcgill/ecse321/rest/dto/CustomerDTO.java
@@ -1,12 +1,70 @@
+
+/*package ca.mcgill.ecse321.rest.dto;
+
+import ca.mcgill.ecse321.rest.models.Customer;
+
+public class CustomerDTO extends PersonDTO{
+
+    private String sportCenterId;
+    public CustomerDTO(Customer customer) {
+        super(customer);
+        this.sportCenterId = customer.getSportCenter().getId();
+    }
+    public String getSportCenterId() {
+        return sportCenterId;
+    }
+
+}*/
+
+
 package ca.mcgill.ecse321.rest.dto;
 
 import ca.mcgill.ecse321.rest.models.Customer;
 
-public class CustomerDTO extends PersonDTO {
+public class CustomerDTO extends PersonDTO{
+
+    private String sportCenter;
+    private String id;
+    private String name;
+    private String password;
+    private String email;
+    private String phoneNumber;
 
     // Constructor accepting a Customer entity
     public CustomerDTO(Customer customer) {
         super(customer);
+        if (customer.getId() != null){
+        this.id = customer.getId();}
+            if (customer.getSportCenter().getId()!= null){
+        this.sportCenter = customer.getSportCenter().getId();}
+                if (customer.getName() != null){
+        this.name = customer.getName();}
+                    if (customer.getPassword()!= null){
+        this. password = customer.getPassword();}
+                        if (customer.getEmail() != null){
+        this.email = customer.getEmail();}
+                            if (customer.getPhoneNumber() != null){
+        this.phoneNumber = customer.getPhoneNumber();}
+
     }
+
+    public String getName(){return this.name;}
+    public String getID(){return this.id;}
+    public String getSportCenter(){return this.sportCenter;}
+    public String getEmail(){return this.email;}
+    public String getPhoneNumber(){return this.phoneNumber;}
+    public String getPassword(){return this.password;}
 }
+
+
+
+
+//
+//    public String getName(){return this.name;}
+//    public String getID(){return this.id;}
+//    public String getSportCenter(){return this.sportCenter;}
+//    public String getEmail(){return this.email;}
+//    public String getPhoneNumber(){return this.phoneNumber;}
+//    public String getPassword(){return this.password;}
+
 
--- src/main/java/ca/mcgill/ecse321/rest/dto/InvoiceDTO.java
+++ src/main/java/ca/mcgill/ecse321/rest/dto/InvoiceDTO.java
@@ -1,17 +1,28 @@
 package ca.mcgill.ecse321.rest.dto;
 
+import ca.mcgill.ecse321.rest.models.Invoice;
+
 public class InvoiceDTO {
 
     private RegistrationDTO registrationDTO;
 
+    private double amount;
+
+    private String id;
     public InvoiceDTO(){};
 
-    public InvoiceDTO(RegistrationDTO registrationDTO){
-        this.registrationDTO = registrationDTO;
+    public InvoiceDTO(Invoice invoice){
+       // this.registrationDTO = registrationDTO;
+        this.id = invoice.getId();
+        this.amount = invoice.getAmount();
     }
 
     public RegistrationDTO getRegistrationDTO(){return registrationDTO;}
 
     public void setRegistrationDTO(RegistrationDTO registrationDTO){this.registrationDTO = registrationDTO;}
+    public void setAmount(int amount){this.amount= amount;}
+    public double getAmount(){return this.amount;}
+    public void setid(String id){this.id= id;}
+    public String getid(){return this.id;}
 
 }
