d0ba6d8982e6f292f4b573aae7d968991f1cfa08
Achraf Ghellach <a.ghellach@gmail.com>
2024-03-23 18:12:12 -0400

    integration tests for authentications

--- src/main/java/ca/mcgill/ecse321/rest/dto/auth/RegisterDTO.java
+++ src/main/java/ca/mcgill/ecse321/rest/dto/auth/RegisterDTO.java
@@ -24,4 +24,22 @@ public class RegisterDTO {
     public String getPhoneNumber() {
         return phoneNumber;
     }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+
 }
