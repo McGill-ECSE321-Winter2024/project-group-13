f865ca7f0ee36c0e58a4a79eba89ece8034c3e4c
TeddyEl-Husseini <143761330+TeddyEl-Husseini@users.noreply.github.com>
2024-03-24 11:33:36 -0400

    Update RegistrationController.java

--- src/main/java/ca/mcgill/ecse321/rest/controllers/RegistrationController.java
+++ src/main/java/ca/mcgill/ecse321/rest/controllers/RegistrationController.java
@@ -1,21 +1,16 @@
 package ca.mcgill.ecse321.rest.controllers;
+
 import ca.mcgill.ecse321.rest.PersonSession;
-import ca.mcgill.ecse321.rest.dto.CourseDTO;
-import ca.mcgill.ecse321.rest.dto.CustomerDTO;
-import ca.mcgill.ecse321.rest.dto.InvoiceDTO;
-import ca.mcgill.ecse321.rest.dto.RegistrationDTO;
+import ca.mcgill.ecse321.rest.dto.*;
 import ca.mcgill.ecse321.rest.helpers.DefaultHTTPResponse;
 import ca.mcgill.ecse321.rest.models.*;
 import ca.mcgill.ecse321.rest.services.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 @CrossOrigin(origins = "*")
@@ -27,6 +22,11 @@ public class RegistrationController {
     @Autowired
     private AuthenticationService authenticationService;
 
+    /**
+     * @Author Teddy El-Husseini
+     * @param bearerToken
+     * @return ResponseEntity
+     */
     @GetMapping(value = { "/registrations", "/registrations/"})
     ResponseEntity<?> getRegistrations(@RequestHeader (HttpHeaders.AUTHORIZATION) String bearerToken){
 
@@ -50,8 +50,13 @@ public class RegistrationController {
            return DefaultHTTPResponse.badRequest("Invalid Credentials!");}
         }
 
-
-@GetMapping(value = {"/registrations/{registration_id}", "/registrations/{registration_id}/"})
+    /**
+     * @Author Teddy El-Husseini
+     * @param registration_id
+     * @param bearerToken
+     * @return ResponseEntity
+     */
+    @GetMapping(value = {"/registrations/{registration_id}", "/registrations/{registration_id}/"})
 public ResponseEntity<?> getSpecificRegistration(@PathVariable String registration_id, @RequestHeader (HttpHeaders.AUTHORIZATION) String bearerToken) {
 
         PersonSession personSession = authenticationService.verifyTokenAndGetUser(bearerToken);
@@ -76,47 +81,55 @@ public ResponseEntity<?> getSpecificRegistration(@PathVariable String registrati
 
 }
 
+    /**
+     * @Author Teddy El-Husseini
+     * @param registration_id
+     * @param bearerToken
+     * @return Boolean
+     */
     @RequestMapping(value = {"/registrations/{registration_id}/cancel","/registrations/{registration_id}/cancel/"} , method = RequestMethod.POST)
     Boolean cancelSpecificRegistration(@PathVariable("registration_id") String registration_id, @RequestHeader (HttpHeaders.AUTHORIZATION) String bearerToken) {
         PersonSession personSession = authenticationService.verifyTokenAndGetUser(bearerToken);
 
         if (personSession.getPersonType().equals(PersonSession.PersonType.Owner)) {
-            Boolean registrations = registrationService.cancelRegistration(registration_id);
-            //if (registrations.equals(false)){return DefaultHTTPResponse.badRequest("Please check RegistrationID");}
-           // return new ResponseEntity<>(registrations, HttpStatus.OK) ;}
-        return registrations;}
+            return registrationService.cancelRegistration(registration_id);}
 
         else if (personSession.getPersonType().equals(PersonSession.PersonType.Customer)){
-            Boolean registrations = registrationService.cancelRegistrationByCustomer(personSession.getPersonId(), registration_id);
-           // if (registrations.equals(false)){return DefaultHTTPResponse.badRequest("Please check CustomerID or RegistrationID");}
-            //return new ResponseEntity<>(registrations, HttpStatus.OK) ;}
-            return registrations;}
+            return registrationService.cancelRegistrationByCustomer(personSession.getPersonId(), registration_id);}
 
         else {
-          //  return DefaultHTTPResponse.badRequest("Invalid Credentials!");}
             return false;}
 
     }
 
+    /**
+     * @Author Teddy El-Husseini
+     * @param registration_id
+     * @param rating
+     * @param bearerToken
+     * @return Boolean
+     */
     @RequestMapping(value = {"/registrations/{registration_id}/{rating}","/registrations/{registration_id}/{rating}/" }, method = RequestMethod.POST)
     Boolean updateRegistrationRating(@PathVariable("registration_id") String registration_id ,@PathVariable("rating") Integer rating, @RequestHeader (HttpHeaders.AUTHORIZATION) String bearerToken) {
         PersonSession personSession = authenticationService.verifyTokenAndGetUser(bearerToken);
 
         if (personSession.getPersonType().equals(PersonSession.PersonType.Customer)){
-            Boolean registrations = registrationService.updateRegistrationRating(personSession,registration_id, rating);
-            //if (registrations.equals(false)){return DefaultHTTPResponse.badRequest("Please check CustomerID, RegistrationID or Rating");}
-            return registrations;}
+            return registrationService.updateRegistrationRating(personSession,registration_id, rating);}
 
         else {
             return false;}
-
     }
 
+    /**
+     * @Author Teddy El-Husseini
+     * @param registration_id
+     * @param bearerToken
+     * @return ResponseEntity
+     */
     @GetMapping(value = {"/registrations/{registration_id}/invoices", "/registrations/{registration_id}/invoices"})
     ResponseEntity<?>  getInvoicesForRegistration(@PathVariable("registration_id") String registration_id, @RequestHeader (HttpHeaders.AUTHORIZATION) String bearerToken) {
 
         PersonSession personSession = authenticationService.verifyTokenAndGetUser(bearerToken);
-
 
         if (personSession.getPersonType().equals(PersonSession.PersonType.Owner)) {
             List<Invoice> registrations = registrationService.getInvoicesForRegistration(registration_id);
@@ -134,14 +147,22 @@ public ResponseEntity<?> getSpecificRegistration(@PathVariable String registrati
 
     }
 
+    /**
+     * Converts Registration to RegistrationDTO
+     * @param registration
+     * @return RegistrationDTO
+     * @Author Teddy El-Husseini
+     */
     private RegistrationDTO convertToDTO(Registration registration){
-
-        CustomerDTO customerDTO = new CustomerDTO(registration.getCustomer());
-        CourseDTO courseDTO = new CourseDTO(registration.getCourse());
-
         return new RegistrationDTO(registration);
     }
 
+    /**
+     * Converts a List of Registrations to a List of RegistrationDTOs
+     * @param registrations
+     * @return List of registrations
+     * @Author Teddy El-Husseini
+     */
     private List<RegistrationDTO> convertRegistrationListToDTO(List<Registration> registrations){
         List<RegistrationDTO> registrationDTOS = new ArrayList<>();
 
@@ -149,9 +170,14 @@ public ResponseEntity<?> getSpecificRegistration(@PathVariable String registrati
             registrationDTOS.add(convertToDTO(r));
         }
         return registrationDTOS;
-
     }
 
+    /**
+     * Converts invoice to invoiceDTO
+     * @param invoice
+     * @return InvoiceDTO
+     * @Author Teddy El-Husseini
+     */
     private InvoiceDTO convertToDTO(Invoice invoice){
 
         InvoiceDTO invoiceDTO = new InvoiceDTO(invoice);
@@ -159,6 +185,12 @@ public ResponseEntity<?> getSpecificRegistration(@PathVariable String registrati
         return invoiceDTO;
     }
 
+    /**
+     * Converts a List of Invoices to a List of InvoicesDTOs
+     * @param invoices
+     * @return List of invoices
+     * @Author Teddy El-Husseini
+     */
     private List<InvoiceDTO> convertInvoiceListToDTO(List<Invoice> invoices){
         List<InvoiceDTO> invoiceDTOS = new ArrayList<>();
 
@@ -166,8 +198,5 @@ public ResponseEntity<?> getSpecificRegistration(@PathVariable String registrati
             invoiceDTOS.add(convertToDTO(i));
         }
         return invoiceDTOS;
-
     }
-
-
 }
