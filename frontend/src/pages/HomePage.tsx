ddc9e93d6ab062df99e851b828aec5457ad92c03
OmarCE11 <omar.moussa@mail.mcgill.ca>
2024-04-07 17:48:46 -0400

    Modifications to CourseTable.tsx

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,16 +1,10 @@
 import React from 'react';
-import CourseTable from '../components/CourseTable'; // Make sure the path is correct
-
-const sampleCourses = [
-    // ... Array of course objects with id, name, description, level, start date, end date, room, instructor, and cost
-];
 
 export default function HomePage() {
     return (
         <main className="py-10">
             <div className="px-4 sm:px-6 lg:px-8">
-                {/* ... other content ... */}
-                <CourseTable courses={sampleCourses} />
+                Home Page!
             </div>
         </main>
     );
