5a87129824c0e80cecfb67f25e7011b2902c6e78
OmarCE11 <omar.moussa@mail.mcgill.ca>
2024-04-07 00:46:28 -0400

    Added a CourseTable.tsx component with filtering and sorting features.

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,29 +1,17 @@
-import { Fragment, useState } from 'react'
-import { Dialog, Menu, Transition } from '@headlessui/react'
-import {
-    Bars3Icon,
-    BellIcon,
-    CalendarIcon,
-    ChartPieIcon,
-    Cog6ToothIcon,
-    DocumentDuplicateIcon,
-    FolderIcon,
-    HomeIcon,
-    UsersIcon,
-    XMarkIcon,
-    RectangleStackIcon
-} from '@heroicons/react/24/outline'
-import { ChevronDownIcon, MagnifyingGlassIcon } from '@heroicons/react/20/solid'
-import User from "../services/user";
+import React from 'react';
+import CourseTable from '../components/CourseTable'; // Make sure the path is correct
 
+const sampleCourses = [
+    // ... Array of course objects with id, name, description, level, start date, end date, room, instructor, and cost
+];
 
 export default function HomePage() {
-
     return (
         <main className="py-10">
             <div className="px-4 sm:px-6 lg:px-8">
-                {User().personEmail}
+                {/* ... other content ... */}
+                <CourseTable courses={sampleCourses} />
             </div>
         </main>
-    )
+    );
 }
