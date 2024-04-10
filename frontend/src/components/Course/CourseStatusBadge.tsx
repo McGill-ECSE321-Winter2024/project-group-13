1b8352665da5855e15b933f10a798d6227920ddf
OmarCE11 <omar.moussa@mail.mcgill.ca>
2024-04-10 15:18:27 -0400

    Minor changes to status icons

--- frontend/src/components/Course/CourseStatusBadge.tsx
+++ frontend/src/components/Course/CourseStatusBadge.tsx
@@ -10,7 +10,7 @@ export default function CourseStatusBadge ({
         [CourseState.Approved]: 'inline-flex items-center rounded-md bg-green-500/10 px-2 py-1 text-xs font-medium text-green-400 ring-1 ring-inset ring-green-500/20',
         [CourseState.AwaitingApproval]: 'inline-flex items-center rounded-md bg-yellow-400/10 px-2 py-1 text-xs font-medium text-yellow-500 ring-1 ring-inset ring-yellow-400/20',
         [CourseState.Denied]: 'inline-flex items-center rounded-md bg-red-400/10 px-2 py-1 text-xs font-medium text-red-400 ring-1 ring-inset ring-red-400/20',
-        [CourseState.Inactive]: 'inline-flex items-center rounded-md bg-green-500/10 px-2 py-1 text-xs font-medium text-green-400 ring-1 ring-inset ring-green-500/20',
+        [CourseState.Inactive]: 'inline-flex items-center rounded-md bg-grey-500/10 px-2 py-1 text-xs font-medium text-grey-400 ring-1 ring-inset ring-grey-500/20',
     };
     return <span className={className[status]}>
     {_.capitalize(status)}
